7ba09a0ce18823a916b678eba6d6e605454e3b18
Alfarizi4 <alfariziulya4@gmail.com>
2022-11-24 09:03:30 +0800

    update kamis

---
+++ app/Http/Controllers/ConntentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConntentController extends Controller
+{
+
+    
+
+{
+    public function index(Request $request)
+    #code...
+}
+
+    public function create(Request $request)
+
+{
+    #code...
+}
+
+    public function store(Request $request)
+
+{
+    #code...
+}
+
+    public function edit(Request $request,konten $konten)
+
+{
+    #code...
+}
+
+    public function update(Request $request,konten $konten)
+
+{
+    #code...
+}
+
+    public function delate(Request $request)
+
+{
+    #code...
+}
+
+    public function index(Request $request)
+
+{
+    #code...
+}
+}
---
+++ resources/views/content.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+@section('content')
+
+<div class="bg-gray-300 ">
+    <div class="max-w-7xl mx-auto bg-white flex flex-col px-10 min-h-screen py-10 gap-10">
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto nisi voluptatibus repellendus adipisci, ipsum, esse libero sit dolorum distinctio, commodi minus explicabo sint facere consequatur odio dolorem nobis. Vero!
+            </span>
+        </div>
+
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto nisi voluptatibus repellendus adipisci, ipsum, esse libero sit dolorum distinctio, commodi minus explicabo sint facere consequatur odio dolorem nobis. Vero!
+            </span>
+        </div>
+
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto nisi voluptatibus repellendus adipisci, ipsum, esse libero sit dolorum distinctio, commodi minus explicabo sint facere consequatur odio dolorem nobis. Vero!
+            </span>
+        </div>
+
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto nisi voluptatibus repellendus adipisci, ipsum, esse libero sit dolorum distinctio, commodi minus explicabo sint facere consequatur odio dolorem nobis. Vero!
+            </span>
+        </div>
+    </div>
+</div>
+    
+@endsection
